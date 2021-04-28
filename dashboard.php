6a9aa7367a54db62196ce74b05d7c56afbcc6a46
MalvikaShetty <shettymalvika11@gmail.com>
2021-04-29 03:12:30 +0530

    dashboard table

--- dashboard.php
+++ dashboard.php
@@ -65,11 +65,11 @@
                             <?php
                             include("connection.php");
                             session_start();
-                            $query = "Select SUM(VolumeOfWater) from Readings WHERE Date='2020-01-02'"; //You don't need a ; like you do in SQL
+                            $query = "Select AVG(VolumeOfWater) from Readings WHERE Date='2020-01-02'"; //You don't need a ; like you do in SQL
                             $result = mysqli_query($con,$query);
                             // $value = mysqli_fetch_object($result);
                             while($row = mysqli_fetch_array($result)){  
-                            echo "<p>" . $row['SUM(VolumeOfWater)'] . ' L' ."</p>" ;
+                            echo "<p>" . $row['AVG(VolumeOfWater)'] . ' L' ."</p>" ;
                             }
                             mysqli_close($con);
                             ?>
@@ -126,7 +126,7 @@
 
 include("connection.php");
 session_start();
-$query = "Select Date,AVG(AverageUse) from (Select Date , SUM(VolumeOfWater) as 'AverageUse' from Readings Group by Date) AS M"; //You don't need a ; like you do in SQL
+$query = "Select Date, SUM(VolumeOfWater) from Readings GROUP BY Date"; //You don't need a ; like you do in SQL
 $result = mysqli_query($con,$query);
 
 echo "<table border='1'>
@@ -139,7 +139,7 @@ while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
 echo "<td>" . $row['Date'] . "</td>";
-echo "<td>" . $row['AVG(AverageUse)'] . "</td>";
+echo "<td>" . $row['SUM(VolumeOfWater)'] . "</td>";
 echo "</tr>";
 }
 echo "</table>";
