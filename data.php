4392a83f3964e11dbda4a93d0448394b139d39d5
MalvikaShetty <shettymalvika11@gmail.com>
2021-04-30 12:16:47 +0530

    line chart with data

--- data.php
+++ data.php
@@ -3,12 +3,14 @@ include("connection.php");
 session_start();
 $query = "Select Date, SUM(VolumeOfWater) from Readings GROUP BY Date"; //You don't need a ; like you do in SQL
 $result = mysqli_query($con,$query);
-if ( ! $result ) {
-    echo mysqli_error($con);
-    die;
-  }
-    
-  echo json_encode($data); 
+$json=[];
+$json2=[];
+while($row = mysqli_fetch_array($result)){  
+  $json =  $row['SUM(VolumeOfWater)'] ;
+  $json2 =  $row['Date'];
+}
+  echo json_encode($json);
+   
   
   mysqli_close($con);
 ?>
